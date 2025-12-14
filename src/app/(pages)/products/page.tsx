c2b06f32dd1393cb2dc165460dad0ce4e4efd421
whatmakeart <whatmakeart@gmail.com>
2025-12-14 20:30:59 +0200

    container of grid view

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -62,7 +62,7 @@ export default function Products() {
 
       {/* Products Grid */}
       <div
-        className={`grid gap-6 ${
+        className={`grid gap-6 container mx-auto${
           viewMode === "grid"
             ? "grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
             : "grid-cols-1"
